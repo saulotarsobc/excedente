c97c432c8d5c35d2b7180f2a96d37aae8ba62eb3
Saulo Costa <saulotarsobc@gmail.com>
2023-08-23 09:11:14 -0300

    :fire:

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll("input");
+const metragem_excedente = document.getElementById("metragem_excedente");
+const valor_metro = document.getElementById("valor_metro");
+const valor_mesalidade = document.getElementById("valor_mesalidade");
+const dias_restantes = document.getElementById("dias_restantes");
+
+const metragem_excedente_resultado = document.getElementById("metragem_excedente_resultado");
+const valor_mensalidade_resultado = document.getElementById("valor_mensalidade_resultado");
+
+inputs.forEach((element) => element.addEventListener("change", () => calc()));
+
+function calc() {
+    /* calc excedente */
+    metragem_excedente_resultado.innerHTML = "R$ " + (metragem_excedente.value * valor_metro.value).toFixed(2);
+
+    /* calc mensalidade proporcional */
+    valor_mensalidade_resultado.innerHTML = "R$ " + ((valor_mesalidade.value / 30) * dias_restantes.value).toFixed(2);
+}
+
+calc();
